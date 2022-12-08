bb530cc0ad0c5cd874c17fb81ee56b3609c912b6
Ishaan Mahajan <ishaanamahajan@gmail.com>
2022-12-07 21:59:25 -0600

    Updated OCR algorithm

--- OCR/ocr.py
+++ OCR/ocr.py
@@ -1,8 +1,22 @@
-import cv2 
+
+import cv2
 import pytesseract
+import numpy as np
 
-img = cv2.imread('image_to_text.jpeg')
+# Load the image using OpenCV
+image = cv2.imread("/Users/ishaanmahajan/VR-Toolkit/OCR/sample1.png")
 
-# Adding custom options
-custom_config = r'--oem 3 --psm 6'
-pytesseract.image_to_string(img, config=custom_config)
+# Convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Apply Gaussian blur to the image
+blur = cv2.GaussianBlur(gray, (3,3), 0)
+
+# Apply adaptive thresholding to the image
+thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 3)
+
+
+text = pytesseract.image_to_string(thresh)
+
+# Print the recognized text
+print(text)
