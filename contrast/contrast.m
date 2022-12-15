9b895943529eed4d47d5a7ef2886a0a824c19cb6
Shivansh Aggarwal <90158094+shivanshagg18@users.noreply.github.com>
2022-12-14 18:22:42 -0600

    Updated contrast code

--- contrast/contrast.m
+++ contrast/contrast.m
@@ -1,48 +1,24 @@
-img = imread('text.png');
+img = imread('scene_input.png');
 fh1 = figure; imshow(img); title('Original');
 
 img_gray = rgb2gray(img);
 
-% imshow(img_gray);
-
-for i=4:6:size(img, 1)-3
-    for j=4:6:size(img, 2)-3
-        minimum = min(min(img_gray(i-3:i+3, j-3:j+3)));
-        maximum = max(max(img_gray(i-3:i+3, j-3:j+3)));
-        if and((((maximum-minimum)*100)/minimum) < 10.0, maximum ~= minimum)
-            img_lower = (maximum - img_gray(i-3:i+3, j-3:j+3)) > (img_gray(i-3:i+3, j-3:j+3) - minimum);
-            for t=-3:3
-                for l=-3:3
-                   if img_lower(t+4, l+4) == 1
-                       img(i+t, j+l, 1) = round(img(i+t, j+l, 1)*0.9);
-                       img(i+t, j+l, 2) = round(img(i+t, j+l, 2)*0.9);
-                       img(i+t, j+l, 3) = round(img(i+t, j+l, 3)*0.9);
-                   else
-                       img(i+t, j+l, 1) = round(img(i+t, j+l, 1)*1.1);
-                       if img(i+t, j+l, 1) > 250
-                           img(i+t, j+l, 1) = 255;
-                       end
-                       img(i+t, j+l, 2) = round(img(i+t, j+l, 2)*1.1);
-                       if img(i+t, j+l, 2) > 250
-                           img(i+t, j+l, 2) = 255;
-                       end
-                       img(i+t, j+l, 3) = round(img(i+t, j+l, 3)*1.1);
-                       if img(i+t, j+l, 2) > 250
-                           img(i+t, j+l, 3) = 255;
-                       end
-                   end
-                end
-            end
+for i=3:size(img, 1)-2
+    for j=3:size(img, 2)-2
+        s = sum(sum(img_gray(i-2:i+2, j-2:j+2) - img_gray(i, j)));
+        if s > 20
+            img(i, j, 1) = img(i, j, 1) + (255-img(i, j, 1))*0.1;
+            img(i, j, 2) = img(i, j, 2) + (255-img(i, j, 2))*0.1;
+            img(i, j, 3) = img(i, j, 3) + (255-img(i, j, 3))*0.1;
+        else
+            img(i, j, 1) = img(i, j, 1)*0.9;
+            img(i, j, 2) = img(i, j, 2)*0.9;
+            img(i, j, 3) = img(i, j, 3)*0.9;
         end
     end
 end
 
-imshow(img);
 
-img(:, :, 1) = medfilt2(img(:, :, 1));
-img(:, :, 2) = medfilt2(img(:, :, 2));
-img(:, :, 3) = medfilt2(img(:, :, 3));
+fh2 = figure; imshow(img);
 
-fh2 = figure; imshow(img); title('Contrast');
-
-saveas(fh2, 'contrast.png');
+imwrite(img, 'scene_output.png');
